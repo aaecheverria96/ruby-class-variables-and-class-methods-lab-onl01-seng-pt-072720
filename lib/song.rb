86c951e0711e7c7da27c656e9198b66b7d02ddcb
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-26 02:57:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,8 @@ end
 
 def self.genre_count
   sorted_genre = {}
-  @@genres.each do |genre,count|
-    sorted_genre[genre] = count
+  @@genres.each do |genre,@@genres.uniq.count|
+    sorted_genre[genre] = @@genres.uniq.count
 end
 sorted_genre
 end
